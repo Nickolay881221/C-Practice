aa8405dc1bd76f8b7f36ac5540ab59d60a63ebec
Nick Volodin <Volod-nick@mail.ru>
2022-10-03 22:16:48 +0300

    Task 3.2 finished

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -5,3 +5,18 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 // 2 -> " -2, -1, 0, 1, 2
 
+int Promt1(string message)
+{
+    System.Console.Write(message);
+    string inputString = Console.ReadLine();
+    int value = Convert.ToInt32(inputString);
+    return value;
+}
+
+int Number = Promt1("Введите число");
+int Count = - Number;
+while (Count <= Number) 
+{
+    System.Console.WriteLine(Count);
+    Count = Count + 1;
+}
